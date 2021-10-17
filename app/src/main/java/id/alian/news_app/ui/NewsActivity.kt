f9b7aa11f092b32da860d61f5959599b6eb70e32
alianhakim8 <alianhakim9@gmail.com>
2021-10-17 14:40:42 +0700

    setting up navigation:

--- app/src/main/java/id/alian/news_app/ui/NewsActivity.kt
+++ app/src/main/java/id/alian/news_app/ui/NewsActivity.kt
@@ -2,6 +2,8 @@ package id.alian.news_app.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import id.alian.news_app.databinding.ActivityNewsBinding
 
 class NewsActivity : AppCompatActivity() {
@@ -13,8 +15,7 @@ class NewsActivity : AppCompatActivity() {
         binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        binding.bottomNavigationView.setupWithNavController
-
+        binding.bottomNavigationView.setupWithNavController(binding.newsNavHostFragment.findNavController())
 
     }
 }
