c587898dcbc0f65dd16479bf4c0de03b103e74fb
alianhakim8 <alianhakim8@gmail.com>
2021-10-18 07:47:45 +0700

    setting up recycler view with diffutil

---
+++ app/src/main/java/id/alian/news_app/adapters/NewsAdapter.kt
@@ -0,0 +1,65 @@
+package id.alian.news_app.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import id.alian.news_app.databinding.ItemArticlePreviewBinding
+import id.alian.news_app.model.Article
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(binding: ItemArticlePreviewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val ivArticleImage = binding.ivArticleImage
+        val tvSource = binding.tvSource
+        val tvTitle = binding.tvTitle
+        val tvDescription = binding.tvDescription
+        val tvPublishedAt = binding.tvPublishedAt
+    }
+
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val itemBinding =
+            ItemArticlePreviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ArticleViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(article.urlToImage).into(holder.ivArticleImage)
+            holder.tvSource.text = article.source.name
+            holder.tvTitle.text = article.title
+            holder.tvDescription.text = article.description
+            holder.tvPublishedAt.text = article.publishedAt
+            setOnItemClickListener {
+                onItemClickListener?.let {
+                    it(article)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+}
--- app/src/main/java/id/alian/news_app/db/ArticleDatabase.kt
+++ app/src/main/java/id/alian/news_app/db/ArticleDatabase.kt
@@ -9,7 +9,8 @@ import id.alian.news_app.model.Article
 
 @Database(
     entities = [Article::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class ArticleDatabase : RoomDatabase() {
